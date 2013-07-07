5eb9264c74e85f7cc72d9e7fd3b62a4c7da3a671
Kevin Peel <kevin@xerodesign.com>
2013-07-07 17:56:34 -0400

    Functions now use @wraps, other misc. changes

--- jsonbouncer/validators.py
+++ jsonbouncer/validators.py
@@ -3,27 +3,33 @@
 """
 
 from __future__ import division, unicode_literals
+from functools import wraps
 
-from jsonbouncer import Invalid, StopValidation, Undefined
+from jsonbouncer import (
+    InvalidBase, Invalid, InvalidGroup, StopValidation, Undefined)
 
 
 def coerce_to(func):
+    @wraps(coerce_to)
     def inner(data):
         if data is Undefined:
             return Undefined
         try:
             return func(data)
         except (ValueError, TypeError):
-            raise Invalid("expected {0}".format(func.__name__))
+            raise Invalid("Expected {0}".format(func.__name__))
     return inner
 
 
 def in_range(minimum=None, maximum=None):
+    @wraps(in_range)
     def inner(data):
+        if data is Undefined:
+            return Undefined
         if minimum is not None and data < minimum:
-            raise Invalid("must be at least {0}".format(minimum))
+            raise Invalid("Must be at least {0}".format(minimum))
         if maximum is not None and data > maximum:
-            raise Invalid("must be no more than {0}".format(maximum))
+            raise Invalid("Must be no more than {0}".format(maximum))
         return data
     return inner
 
@@ -41,32 +47,36 @@ def maximum(value, inclusive=True):
 
 
 def in_list(allowed):
+    @wraps(in_list)
     def inner(data):
+        if data is Undefined:
+            return Undefined
         if data not in allowed:
-            raise Invalid("not in list of valid values")
+            raise Invalid("Not in list of valid values")
         return data
     return inner
 
 
 def require_one(*fields):
+    @wraps(require_one)
     def inner(data):
         working_data = data
         is_invalid = False
-        if isinstance(data, Invalid):
+        if isinstance(data, InvalidGroup):
             working_data = data.data
             is_invalid = True
         if not isinstance(working_data, dict):
-            return Invalid("Expected an object")
+            raise Invalid("Expected an object")
         valid_field = None
         for field in fields:
-            if not isinstance(working_data[field], Invalid):
+            if not isinstance(working_data[field], InvalidBase):
                 valid_field = field
                 break
         if valid_field:
             for field in fields:
                 if field == valid_field:
                     continue
-                if is_invalid and isinstance(working_data[field], Invalid):
+                if is_invalid and isinstance(working_data[field], InvalidBase):
                     data.errors.remove(working_data[field])
                 del working_data[field]
         if is_invalid and data.errors:
@@ -77,17 +87,18 @@ def require_one(*fields):
 
 
 def require_if(field, func):
+    @wraps(require_if)
     def inner(data):
         working_data = data
         is_invalid = False
-        if isinstance(data, Invalid):
+        if isinstance(data, InvalidBase):
             working_data = data.data
             is_invalid = True
         if not isinstance(working_data, dict):
-            return Invalid("Expected an object")
+            raise Invalid("Expected an object")
         is_required = func(working_data)
         if not is_required:
-            if is_invalid and isinstance(working_data[field], Invalid):
+            if is_invalid and isinstance(working_data[field], InvalidBase):
                 data.errors.remove(working_data[field])
             del working_data[field]
         if is_invalid and data.errors:
@@ -98,6 +109,7 @@ def require_if(field, func):
 
 
 def when_empty(func):
+    @wraps(when_empty)
     def inner(data):
         if not bool(data):
             raise StopValidation(_when_base(data, func))
@@ -106,6 +118,7 @@ def when_empty(func):
 
 
 def when_zero(func):
+    @wraps(when_zero)
     def inner(data):
         if data == 0 and type(data) != bool:
             raise StopValidation(_when_base(data, func))
@@ -114,6 +127,7 @@ def when_zero(func):
 
 
 def when_false(func):
+    @wraps(when_false)
     def inner(data):
         if data is False:
             raise StopValidation(_when_base(data, func))
@@ -122,6 +136,7 @@ def when_false(func):
 
 
 def when_none(func):
+    @wraps(when_none)
     def inner(data):
         if data is None:
             raise StopValidation(_when_base(data, func))
@@ -130,6 +145,7 @@ def when_none(func):
 
 
 def when_empty_str(func):
+    @wraps(when_empty_str)
     def inner(data):
         if data == "":
             raise StopValidation(_when_base(data, func))
@@ -138,6 +154,7 @@ def when_empty_str(func):
 
 
 def when_undefined(func):
+    @wraps(when_undefined)
     def inner(data):
         if data is Undefined:
             raise StopValidation(_when_base(data, func))
